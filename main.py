a5c86b39edc40049c791ad1af5dce6a848aa4dbc
Nikhil Kumar <nikhilkkumar247@gmail.com>
2024-08-10 22:35:20 -0700

    add spin function

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+import random 
+
 MAX_LINES = 5
 MIN_BET = 10
 MAX_BET = 50
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns  = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
 
 def deposit():
     while True:
@@ -38,7 +69,7 @@ def get_bet():
             if MIN_BET <= amount <= MAX_BET:
                 break
             else:
-                print(f"Bet must be between ${MIN_BET} - {MAX_BET}.")
+                print(f"Bet must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("Please enter a number")
 
@@ -47,7 +78,16 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    print(balance,lines,bet)
+    while True:
+        bet = get_bet()
+        total_bet = lines*bet
+
+        if total_bet > balance:
+            print(f"You are betting ${total_bet} with an insufficient balance of ${balance}.")
+        else:
+            break
     
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    # print(balance,lines,bet)
+
 main()
